42a6bb40f7793444a66f911325873234a96e428e
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2022-09-17 22:32:53 +0200

    fixup! Add Dockerfiles and GitHub workflows generators

--- tools/generators/gen/globals.py
+++ tools/generators/gen/globals.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+from jinja2 import pass_eval_context
+
 from .decorators import JinjaGlobal as jinja_global
 
 
@@ -9,6 +11,20 @@ def _global_random_string(length=8, alphabet=None):
     if not alphabet:
         alphabet = string.ascii_letters + string.octdigits
     return "".join(list(map(lambda _: random.choice(alphabet), range(length))))
+
+
+@jinja_global
+@pass_eval_context
+def _global_counter_string(ctx, length=4):
+    self = _global_counter_string
+    if not hasattr(self, "count") or id(ctx) != self.ctx_id:
+        self.count = 0
+        self.ctx_id = id(ctx)
+
+    count = self.count
+    self.count += 1
+
+    return "{:0{n}d}".format(count, n=length)
 
 
 @jinja_global(type=jinja_global.VARIABLE)
